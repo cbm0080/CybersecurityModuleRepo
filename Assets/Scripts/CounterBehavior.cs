312b7e7c83e3568774f66a4fdf5c99c3e37908bd
Christian Marlow <cbm0080@auburn.edu>
2025-04-29 16:48:42 -0500

    Added intro audio + Continue Button
    The continue button currently doesn't have any behavior attributed to it. I figured it would take the player back to the home screen, but I figure we can sort that out when putting it all together.

--- Assets/Scripts/CounterBehavior.cs
+++ Assets/Scripts/CounterBehavior.cs
@@ -1,31 +1,40 @@
 using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CounterBehavior : MonoBehaviour
 {
     public TextMeshProUGUI text;
     private int _count;
+    private bool _updated;
+
+    public Button continueButton;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         text.SetText($"{_count}/6 Errors Found");
+        _updated = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (_count == 6)
-        {
-            text.SetText("Click to continue");
-        }   
+          
     }
 
     public void IncrementCounter()
     {
-        if (_count <= 6)
+        if (_count < 6)
         {
             _count++;
             text.SetText($"{_count}/6 Errors Found");
+            
+            if (_count == 6 && _updated == false)
+            {
+                continueButton.interactable = true;
+                continueButton.GetComponent<CanvasGroup>().alpha = 1;
+            }
         }
+         
     }
 }
